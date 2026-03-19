41b03a5fd00548e602ef55bb27d8ce5a3e49d6c8
Alex <alekssaakan@mail.ru>
2026-03-19 22:43:15 +0300

    Всё просто

--- Семинар 2/Task 10_innocents_n_suspects.py
+++ Семинар 2/Task 10_innocents_n_suspects.py
@@ -1,2 +1,8 @@
 innocents = [1, 5, 9, 10, 33, 70]
 suspects = [2, 5, 7, 10, 23, 105]
+clean_list = []
+for i in innocents:
+    if i not in suspects:
+        clean_list.append(i)
+print(clean_list)
+    
